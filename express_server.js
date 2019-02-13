483cf4e252b1ada1d908b1fea1b4cfbb24df00de
John <connollyjohnf@gmail.com>
2019-02-13 22:05:06 +0000

    removed unneccesary console.logs, added comments

--- express_server.js
+++ express_server.js
@@ -4,19 +4,17 @@ var PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// creates shortened URL string
 function generateRandomString() {
   return Math.random()
     .toString(36)
     .substr(2, 6);
 }
 
-// Add the code below
-// to the top of the express_server.js file.
-// After we've declared app, as shown below.
-// This tells the Express app to use EJS as its
-// templating engine.
+// tells the Express app to use EJS as its templating engine.
 app.set("view engine", "ejs");
 
+// starting database
 var urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -29,63 +27,67 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//go to New page, renders the new.ejs
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// newly added URL (a new value) is given a Random String as a key.
+// Both are inserted to the urlDatabase
+// redirects to main url listing page
 app.post("/urls", (req, res) => {
-  // console.log(req.body); // Log the POST request body to the console
   var short = generateRandomString();
   var long = req.body.longURL;
   urlDatabase[short] = long;
-
-  // console.log(urlDatabase);
-  const longURL = req.body.longURL;
   res.redirect("/urls/" + short);
 });
 
+// root page which redirects to URL Listing Page
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("/urls");
 });
 
 app.get("urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World!</b></body></html>\n");
-});
-
+// see an individual URL's page
+// pass through the long and short URLs to the page so they
+// can be used there
 app.get("/urls/:shortURL", (req, res) => {
-  // console.log(req.params);
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+// on single URL page, linked URL will
+// send GET request to redirect
+// user to longURL
 app.get("/u/:shortURL", (req, res) => {
-  // console.log(req);
   let shortURL = req.params.shortURL;
   let longURL = urlDatabase[shortURL];
+
+  // if there is no "http://" at the beginning of the
+  // longURL, this will add it.
   if (!longURL.startsWith("http://")) {
     longURL = "http://" + longURL;
   }
   res.redirect(longURL);
 });
 
+// Updating a url, but keep the short url key
 app.post("/urls/:shortURL", (req, res) => {
   let newLong = req.body.longURL;
   urlDatabase[req.params.shortURL] = newLong;
   res.redirect("/urls");
 });
 
+// deletes a specific key/value from the urlDatabase
 app.post("/urls/:shortURL/delete", (req, res) => {
-  // find index of url and splice it out, then redirect to main url list
-  // const index = Object.keys(urlDatabase);
-  // console.log(req.params);
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
 
+// if app can run, console log will print a confirmation
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
