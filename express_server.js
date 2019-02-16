09d9b38cbbd37e64b78c3bba8bf8f33eddf0f78c
John <connollyjohnf@gmail.com>
2019-02-16 02:48:06 +0000

    removed unneccesary code, added relevant comments

--- express_server.js
+++ express_server.js
@@ -4,35 +4,25 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 const cookieSession = require("cookie-session");
+const bcrypt = require("bcrypt");
 app.use(
   cookieSession({
     name: "user_id",
     keys: ["1", "2"]
   })
 );
+app.set("view engine", "ejs");
 
-const bcrypt = require("bcrypt");
-// const password = "purple-monkey-dinosaur"; // found in the req.params
-// const hashedPassword = bcrypt.hashSync(password, 10);
-
-// creates shortened URL string
+// creates random ID string
 function generateRandomString() {
   return Math.random()
     .toString(36)
     .substr(2, 6);
 }
 
+//finds and returns the shortURL object with all the
 function getUrlObject(shortURL) {
   return urlDatabase[shortURL];
-}
-
-// checks the user db for a correct and previously existing email and password.
-function authenticateUser(email, password) {
-  for (let key in users) {
-    if (users[key].email === email && users[key].password === password) {
-      return users[key];
-    }
-  }
 }
 
 //filters and displays the URLs by the User ID
@@ -46,16 +36,13 @@ function urlsForUser(id) {
   return urls;
 }
 
-// tells the Express app to use EJS as its templating engine.
-app.set("view engine", "ejs");
-
 // starting URL database
 const urlDatabase = {
   b2xVn2: { longURL: "http://www.lighthouselabs.ca", userID: "id1" },
   "9sm5xK": { longURL: "http://www.google.com", userID: "id2" }
 };
 
-// User Database
+// Starting User Database
 const users = {
   id1: {
     id: "id1",
@@ -75,6 +62,7 @@ app.get("/urls", (req, res) => {
     let id = req.session.user_id;
     let email = users[id].email;
     if (id) {
+      // find URLs created by the User and send them to be rendered on the page
       let urls = urlsForUser(id);
       let templateVars = { urls: urls, id: id, email: email };
       res.render("urls_index", templateVars);
@@ -115,28 +103,25 @@ app.get("urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-// see an individual URL's page.
-// pass through the long and short URLs to the page so they
-// can be used there
+// see an individual URL's page. show their long and short URLs to the page so they can be accessed there
 app.get("/urls/:shortURL", (req, res) => {
   let id = req.session.user_id;
   if (id) {
+    //if there's a session cookie, render the page
     let email = users[id].email;
     let templateVars = { email: email, id: id, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL };
     res.render("urls_show", templateVars);
   } else {
+    // no cookie? no page.
     res.status(403).send('<p>Invalid email or password</p><a href="/login">Go Back</a>');
   }
 });
 
-// on single URL page, linked URL will
-// send GET request to redirect
-// user to longURL
+// on solo URL page, linked URL will send GET request to redirect user to longURL
 app.get("/u/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
   let longURL = urlDatabase[shortURL].longURL;
-  // if there is no "http://" at the beginning of the
-  // longURL, this will add it.
+  // if there is no "http://" at the beginning of the longURL, this will add it.
   if (!longURL.startsWith("http://")) {
     longURL = "http://" + longURL;
   }
@@ -150,22 +135,25 @@ app.get("/register", (req, res) => {
   res.render("registration", templateVars);
 });
 
-// newly added URL (a new value) is given a Random String as a key.
-// Both are inserted to the urlDatabase
-// redirects to main url listing page
+// new URL is given a Random String as its key and given values of the Creator's ID and LongURL
+// all are inserted to the urlDatabase
 app.post("/urls", (req, res) => {
   let short = generateRandomString();
   let long = req.body.longURL;
   let id = req.session.user_id;
+  // key is random string, longURL is the complete URL, id is the user's id
   urlDatabase[short] = { longURL: long, userID: id };
   res.redirect("/urls/" + short);
 });
 
 // Assigned user can update their urls, but keep the short url key
 app.post("/urls/:shortURL", (req, res) => {
+  // checks if cookie id matches user id
   if (!getUrlObject(req.params.shortURL.userID) === req.session.user_id) {
+    // match FAILS
     res.status(403).send('<p>Invalid email or password</p><a href="/urls">Go Back</a>');
   } else {
+    // match SUCCESS
     let newLong = req.body.longURL;
     urlDatabase[req.params.shortURL].longURL = newLong;
     res.redirect("/urls");
@@ -174,10 +162,12 @@ app.post("/urls/:shortURL", (req, res) => {
 
 // only the owner can delete a specific Short URL listing
 app.post("/urls/:shortURL/delete", (req, res) => {
-  let currentUser = { id: req.session.user_id };
+  // checks if cookie id matches user id
   if (!getUrlObject(req.params.shortURL).userID === req.session.user_id) {
+    // match FAILS
     res.status(403).send('<p>Invalid email or password</p><a href="/login">Go Back</a>');
   } else {
+    // match SUCCESS
     delete urlDatabase[req.params.shortURL];
     res.redirect("/urls");
   }
@@ -188,15 +178,16 @@ app.post("/login", (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
   for (var key in users) {
+    //checks user Database to see if both email and pw match
     if (email === users[key].email && bcrypt.compareSync(password, users[key].password)) {
       req.session.user_id = key;
       return res.redirect("/urls");
     }
-  }
+  } // if loop above finishes with no result...
   res.status(403).send('<p>Invalid email or password</p><a href="/login">Go Back</a>');
 });
 
-// Sending new user to the User Database
+// Creating and sending new user to the User Database
 app.post("/register", (req, res) => {
   // if either email or password is left blank, return error
   const hashedPassword = bcrypt.hashSync(req.body.password, 10);
@@ -212,7 +203,7 @@ app.post("/register", (req, res) => {
       }
     }
     if (!found) {
-      // only runs if found is False
+      // only runs if found is False. This person doesn't exist! Make em!
       let id = generateRandomString();
       users[id] = { id: id, email: req.body.email, password: hashedPassword };
       req.session.user_id = id;
